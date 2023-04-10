0c7d99426125cbc36399e82175d423e6951041b7
Andrew Lund <andrew.s.lund@gmail.com>
2023-04-10 11:36:01 -0700

    completed BMRs

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // TODO
+const { useState } = React;
+
+const GroceryList = (props) => (
+    <ul>
+      {props.groceries.map((grocery) => (
+        <GroceryListItem grocery={grocery}  />
+      ))}
+    </ul>
+)
+
+const GroceryListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+  const style = {
+    fontWeight: isDone ? 'bold' : 'none'
+  };
+
+  return (
+    <li style={style} onMouseOver={() => setIsDone(!isDone)}>{props.grocery}</li>
+  )
+}
+
+
+
+const App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceries={['Tide Pods', 'Nuggies']} />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("app"));
